c1e0542612f389c2b9394fa8a690f58f2719833e
Kyle Lin <linkyle0924@gmail.com>
2025-10-10 17:00:35 -0700

    feat: add div with DOM

--- src/blog.js
+++ src/blog.js
@@ -1,13 +1,4 @@
-type Blog = {
-    title: string;
-    date: string;
-    description: string;
-    image: string;
-    imageAlt: string;
-    slug: string;
-}
-
-const blogs: Blog[] = [
+var blogs = [
     {
         title: "Mid Autumn Festival",
         date: "October 6th, 2025",
@@ -20,41 +11,32 @@ const blogs: Blog[] = [
         title: "First Week of School",
         date: "September 24th, 2025",
         description: "My first week went well! I'm not used to living off campus, but it's not too bad. The bus comes really often and is pretty consistent. Even if I do miss the bus, I can also always walk to school because the walk to school is only thirty minutes.",
-        image: "assets/blog1.jpeg",
+        image: "/assets/blog1.jpeg",
         imageAlt: "Blog Photo",
         slug: ""
     }
-]
-
-const blogContainer = document.getElementById('blog-container');
-
-blogs.forEach((blog) => {
-    const blogDiv = document.createElement("div");
+];
+var blogContainer = document.getElementById('blog-container');
+blogs.forEach(function (blog) {
+    var blogDiv = document.createElement("div");
     blogDiv.className = "blog-post";
-
-    const blogTitle = document.createElement("h1");
+    var blogTitle = document.createElement("h1");
     blogTitle.className = "blog-title";
     blogTitle.textContent = blog.title;
-
-    const blogDate = document.createElement("p");
+    var blogDate = document.createElement("p");
     blogDate.className = "blog-date";
     blogDate.textContent = blog.date;
-
-    const blogDescription = document.createElement("p");
+    var blogDescription = document.createElement("p");
     blogDescription.className = 'blog-description';
     blogDescription.textContent = blog.description;
-
-    const blogImage = document.createElement("img");
+    var blogImage = document.createElement("img");
     blogImage.src = blog.image;
     blogImage.alt = blog.imageAlt;
-
     blogDiv.appendChild(blogTitle);
     blogDiv.appendChild(blogDate);
     blogDiv.appendChild(blogDescription);
     blogDiv.appendChild(blogImage);
-
-    if (blogContainer){
+    if (blogContainer) {
         blogContainer.appendChild(blogDiv);
     }
-
-})
+});
