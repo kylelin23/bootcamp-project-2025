2e4fa79bd7fcc5d4e343d2775fde148af128767a
Kyle Lin <linkyle0924@gmail.com>
2025-12-10 01:08:49 -0800

    feat: make POST endpoint

--- bootcamp-milestone-4/src/app/api/blog/[slug]/comment/route.ts
+++ bootcamp-milestone-4/src/app/api/blog/[slug]/comment/route.ts
@@ -1,24 +1,41 @@
 import connectDB from "@/src/database/db";
 import Blog from "@/src/database/blogSchema";
 import { NextRequest, NextResponse } from "next/server";
+import { Noto_Sans_Old_Hungarian } from "next/font/google";
 
 
 export async function POST(req: NextRequest, { params }: { params: Promise<{ slug: string }> }) {
 
     const { slug } = await params;
 
-    await connectDB();
-
     const body = await req.json();
+    const { user, comment } = body;
 
-
-    if (!body || !body.author || !body.comment) {
+    if (!user || !comment || typeof user != "string" || typeof comment != "string"){
         return NextResponse.json(
-            { error: "Invalid comment data" },
+            { error: "Invalid body of comment" },
             { status: 400 }
         );
     }
 
-	// push comment object to document
-	Blog.update(...)
+    await connectDB();
+
+	const newComment = {
+        user,
+        comment,
+        time: new Date(),
+    };
+
+    const updatedBlog = await Blog.findOneAndUpdate(
+        { slug: slug },
+        { $push: { comments: newComment } },
+        { new: true } // return updated doc
+    );
+
+    if (!updatedBlog) {
+        return NextResponse.json(
+            { error: "Blog not found" },
+            { status: 404 }
+        );
+    }
 }
